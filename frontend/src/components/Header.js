2a3319e16846c51318a6825ad229fc376c316b12
unknown <sagrawal9689@gmail.com>
2021-10-11 23:33:24 +0530

    Added Header component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,33 @@
 import React from 'react'
+import { Navbar, Nav, Container, Button } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <header>
+            <Navbar bg='primary' variant='dark' expand='lg' collapseOnSelect>
+              <Container>
+                <LinkContainer to='/'>
+                  <Navbar.Brand>Tasks App</Navbar.Brand>
+                </LinkContainer>
+                <div style={{display: 'flex'}}>
+                <Button variant="light" className="mx-4">    
+                    Add Task
+                </Button>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                  <Nav className='ms-auto'>
+                    <LinkContainer to='/login'>
+                      <Nav.Link>
+                        Sign In
+                      </Nav.Link>
+                    </LinkContainer>
+                  </Nav>
+                </Navbar.Collapse>
+                </div>
+              </Container>
+            </Navbar>
+      </header>
     )
 }
 
