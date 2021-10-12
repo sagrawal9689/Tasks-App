28b8c6c731214c7a8e51cf2dbd16093744bfa8ba
unknown <sagrawal9689@gmail.com>
2021-10-12 19:28:47 +0530

    Created backend routes for task creat, update and delete

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,11 +2,13 @@ import express from 'express'
 const router = express.Router()
 import {
     createTask,
-    getTasks
+    getTasks,
+    updateTask,
+    deleteTask
 } from '../controllers/taskController.js'
 import { protect } from '../middlewares/authMiddleware.js'
 
 
-router.route('/').get(protect,getTasks).post(protect,createTask)
+router.route('/').get(protect,getTasks).post(protect,createTask).patch(protect,updateTask).delete(protect,deleteTask)
 
 export default router
